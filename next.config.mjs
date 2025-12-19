7e9be724e044d81cd43a319d286dd409031da175
chihpao <chihpaoo@gmail.com>
2025-12-19 18:01:25 +0800

    刪除警告

--- next.config.mjs
+++ next.config.mjs
@@ -3,6 +3,13 @@ import createNextIntlPlugin from 'next-intl/plugin';
 const withNextIntl = createNextIntlPlugin('./src/i18n/request.ts');
 
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+  webpack: (config) => {
+    config.infrastructureLogging = {
+      level: 'error',
+    };
+    return config;
+  },
+};
 
 export default withNextIntl(nextConfig);
