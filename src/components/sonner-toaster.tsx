94460cf8defa83a87775d06eb4cda1600a7e07b3
chihpao <chihpaoo@gmail.com>
2026-01-12 17:59:33 +0800

    狀態更新

--- src/components/sonner-toaster.tsx
+++ src/components/sonner-toaster.tsx
@@ -1,7 +1,21 @@
 'use client';
 
+import { useEffect, useState } from 'react';
+import { createPortal } from 'react-dom';
+
 import { Toaster } from '@/lib/sonner';
 
 export const SonnerToaster = () => {
-  return <Toaster richColors closeButton position="bottom-right" theme="light" />;
+  const [mounted, setMounted] = useState(false);
+
+  useEffect(() => {
+    setMounted(true);
+  }, []);
+
+  if (!mounted) return null;
+
+  return createPortal(
+    <Toaster richColors closeButton position="bottom-right" theme="light" />,
+    document.body
+  );
 };
